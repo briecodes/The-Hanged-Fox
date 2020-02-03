ffff2fde784d64f9f4657b0f82c872ce30305c28
briecodes <hello@brie.work>
2020-02-03 12:23:13 -0500

    Adding game end functionality.

--- src/Containers/GameBoard/GameBoard.js
+++ src/Containers/GameBoard/GameBoard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 import './GameBoard.css';
 
@@ -9,27 +9,33 @@ import LetterBank from '../../Components/LetterBank/LetterBank';
 export default function GameBoard(props) {
   const [helpOverlay, setHelpOverlay] = useState(true);
   const [usedBank, setUsedBank] = useState([]);
+  const incorrect = useRef();
+  const tries = useRef();
   const word = 'success';
 
   function handleLetterPress(letter) {
-    const exists = usedBank.find( (l) => l === letter);
+    setUsedBank([...usedBank, letter]);
 
-    if (!exists) {
-      setUsedBank([...usedBank, letter]);
+    if (!word.includes(letter)) {
+      if (!incorrect.current) incorrect.current = 0;
+      incorrect.current = incorrect.current + 1;
+    } else {
+      if (!tries.current) tries.current = 0;
+      tries.current = tries.current + 1;
     };
   };
 
   return (
     <div className='game-board'>
-      {helpOverlay ? <HelpOverlay closeOverlay={setHelpOverlay} /> : null}
+
       <div className='nav'>
         <div className='nav-btn home' onClick={() => props.goHome('home')}></div>
         <div className='nav-btn help' onClick={() => setHelpOverlay(true)}></div>
-
-        <MysteryWord word={word} usedBank={usedBank} />
-
-        <LetterBank handleLetterPress={handleLetterPress} word={word} usedBank={usedBank} />
       </div>
+
+      {helpOverlay ? <HelpOverlay closeOverlay={setHelpOverlay} /> : null}
+      <MysteryWord word={word} usedBank={usedBank} />
+      { word.length <= incorrect.current ? <div>You Lose!!</div> : <LetterBank handleLetterPress={handleLetterPress} word={word} usedBank={usedBank} />}
     </div>
   );
 };
