e0b24c5ce5d150fc79264c5aeffb89d59eeac1f3
briecodes <hello@brie.work>
2020-02-03 09:43:10 -0500

    Updated help overlay with instruction bubbles.

--- src/Components/HelpOverlay/HelpOverlay.js
+++ src/Components/HelpOverlay/HelpOverlay.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import './HelpOverlay.css';
 
+import instructionsA from '../../Assets/Images/instruction-bubbles-01.png';
+import instructionsB from '../../Assets/Images/instruction-bubbles-02.png';
+
 export default function HelpOverlay(props) {
   return (
     <div className='help-overlay'>
       <div className='close-overlay' onClick={() => props.closeOverlay(false)}></div>
+      <img src={instructionsA} alt='instructions a' />
+      <img src={instructionsB} alt='instructions b' />
     </div>
   );
 };
