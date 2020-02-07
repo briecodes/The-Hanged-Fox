95733a77773e15089d5577e6537f21f40a8409a8
briecodes <hello@brie.work>
2020-02-07 18:24:37 -0500

    Updated fox smoothness.

--- src/Components/HangedFox/HangedFox.js
+++ src/Components/HangedFox/HangedFox.js
@@ -4,6 +4,14 @@ import './HangedFox.css';
 
 export default function HangedFox(props) {
   return (
-    <div className={`hanged-fox-container stage${props.stage ? props.stage : 0}`}></div>
+    // <div className={`hanged-fox-container stage${props.stage ? props.stage : 0}`}></div>
+    <>
+      <div className={`hanged-fox-container stage0 ${props.stage === 0 || props.stage === undefined ? null : 'hide'}`}></div>
+      <div className={`hanged-fox-container stage1 ${props.stage === 1 ? null : 'hide'}`}></div>
+      <div className={`hanged-fox-container stage2 ${props.stage === 2 ? null : 'hide'}`}></div>
+      <div className={`hanged-fox-container stage3 ${props.stage === 3 ? null : 'hide'}`}></div>
+      <div className={`hanged-fox-container stage4 ${props.stage === 4 ? null : 'hide'}`}></div>
+      <div className={`hanged-fox-container stage5 ${props.stage === 5 ? null : 'hide'}`}></div>
+    </>
   );
 };
