67e1646ffcef33080251dc61f07e0e1dbb86bc7a
briecodes <hello@brie.work>
2020-02-08 03:51:45 -0500

    Small animations added.

--- src/Components/HelpOverlay/HelpOverlay.js
+++ src/Components/HelpOverlay/HelpOverlay.js
@@ -8,9 +8,9 @@ import instructionsB from '../../Assets/Images/instruction-bubbles-02.png';
 export default function HelpOverlay(props) {
   return (
     <div className='help-overlay' onClick={props.closeOverlay}>
-      <div className='close-overlay'></div>
-      <img src={instructionsA} alt='instructions a' />
-      <img src={instructionsB} alt='instructions b' />
+      <div className='close-overlay' onClick={props.closeOverlay}></div>
+      <img src={instructionsA} alt='instructions a' onClick={props.closeOverlay} />
+      <img src={instructionsB} alt='instructions b' onClick={props.closeOverlay} />
     </div>
   );
 };
