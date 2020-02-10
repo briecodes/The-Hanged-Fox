e1ab9f77ac2f6a4e507de6d194ca179725a65d19
briecodes <hello@brie.work>
2020-02-10 11:38:54 -0500

    Removed unused line. Updated counter.

--- src/Containers/GameBoard/GameBoard.js
+++ src/Containers/GameBoard/GameBoard.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 import './GameBoard.css';
 
@@ -17,13 +17,19 @@ export default function GameBoard(props) {
   const [gameEnd, setGameEnd] = useState({end: false, won: null});
   const incorrectCount = useRef();
   const correctCount = useRef();
-  const word = 'success';
-  const hint = 'The blood is rare and sweet as cherry wine.';
+  const word = useRef();
+  const hint = useRef();
+  // const word = 'success';
+  // const hint = 'The blood is rare and sweet as cherry wine.';
+
+  useEffect(() => {
+    fetchWordApi();
+  }, []);
 
   function handleLetterPress(letter) {
     setUsedBank([...usedBank, letter]);
 
-    if (!word.includes(letter)) {
+    if (!word.current.includes(letter)) {
       if (!incorrectCount.current) incorrectCount.current = 0;
       incorrectCount.current = incorrectCount.current + 1;
       if (5 <= incorrectCount.current) setGameEnd({end: true, won: false});
@@ -39,10 +45,12 @@ export default function GameBoard(props) {
     setShowHint(false);
     setGameEnd({end: false, won: null});
     incorrectCount.current = 0;
+    correctCount.current.current = 0;
+    fetchWordApi();
   };
 
   function wordLength() {
-    const arr = word.split('');
+    const arr = word.current.split('');
     const finalArr = Array.from(new Set(arr));
 
     return finalArr.length;
@@ -53,24 +61,41 @@ export default function GameBoard(props) {
     setHelpOverlay(!helpOverlay);
   };
 
+  function fetchWordApi() {
+    fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true', {
+      'method': 'GET',
+      'headers': {
+        'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+        'x-rapidapi-key': '46fac88d67msh5d61d8e332a2b09p178748jsn53951fc968fc'
+      }
+    })
+    .then( response => response.json() )
+    .then(res => {
+      word.current = res.word;
+      hint.current = res.results[0].definition;
+    })
+    .catch(err => {
+      console.log('error: ', err);
+    });
+  };
+
   return (
     <div className='game-board'>
       <Nav home={() => props.goHome('home')} help={() => setHelpOverlay(true)} />
       <Tries tries={incorrectCount.current} />
 
-      {/* {helpOverlay || props.showInstructions ? <HelpOverlay closeOverlay={handleHelpOverlay} /> : null} */}
       <HelpOverlay closeOverlay={handleHelpOverlay} showOverlay={helpOverlay || props.showInstructions} />
 
       <HangedFox stage={incorrectCount.current} />
-      <MysteryWord word={word} usedBank={usedBank} />
+      {word.current ? <MysteryWord word={word.current} usedBank={usedBank} /> : null}
       
       { gameEnd.end && !gameEnd.won ? <div className='endgame-message'>You Lose!!</div> : null}
       { gameEnd.won ? <div className='endgame-message'>Congrats!</div> : null }
-      { !gameEnd.end ? <LetterBank handleLetterPress={handleLetterPress} word={word} usedBank={usedBank} /> : null}
+      { !gameEnd.end && word.current ? <LetterBank handleLetterPress={handleLetterPress} word={word.current} usedBank={usedBank} /> : null}
 
       <div className='hint-container'>
         { !gameEnd.end ? <button className='hint' onClick={() => setShowHint(true)}>Hint</button> : <button className='hint' onClick={resetGame}>Play Again</button> }
-        <HintOverlay hint={hint} closeHandler={() => setShowHint(false)} showHint={showHint} />
+        {hint.current ? <HintOverlay hint={hint.current} closeHandler={() => setShowHint(false)} showHint={showHint} /> : null}
       </div>
     </div>
   );
